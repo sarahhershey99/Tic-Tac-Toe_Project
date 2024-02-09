f91aec7ad557b499d5a3caeed65e82b76edf4c80
sarahhershey99 <150100089+sarahhershey99@users.noreply.github.com>
2024-02-08 17:13:24 -0700

    updated

--- TTT.js
+++ TTT.js
@@ -2,6 +2,14 @@ const playerO = "O";
 const playerX = "X";
 let currentPlayer = playerO;
 let gameEnded = false;
+const board = document.getElementById("board");
+let winCombos = [
+    [1, 2, 3], [4, 5, 6], 
+    [7, 8, 9], [1, 4, 7], 
+    [2, 5, 8], [3, 6, 9], 
+    [1, 5, 9], [3, 5, 7]
+  ];
+const resetBtn = document.getElementById("resetButton");
 let squares = document.getElementsByClassName("sqr");
 function swapTurns(){
     if (currentPlayer === playerO){
@@ -12,8 +20,26 @@ function swapTurns(){
 };
 for (let i = 0; i < squares.length; i++){
     squares[i].addEventListener('click', (e) =>{
-        e.target.textContent = currentPlayer;
+        squares[i].textContent = currentPlayer;
         swapTurns();
         console.log("clicked");
     });
 };
+function checkWin(){
+    for (let i = 0; i < winCombos.length; i++) {
+    if (currentPlayer === playerO && winCombos == true){
+        prompt("Congrats playerO! You've won!");
+    } else if (currentPlayer === playerX && winCombos == true){
+        prompt("Congrats playerX! You've won!");
+    };
+};
+};
+
+function reset(){
+    for (let i = 0; i < squares.length; i++){
+    
+    };
+};
+resetBtn.addEventListener('click', (e)=>{
+    reset();
+});
